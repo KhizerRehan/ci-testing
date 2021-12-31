a01cddc1b682351c8e397640d0a103c21f12f94f
khizerrehan <khizerrehan92@gmail.com>
2021-12-31 15:45:58 +0500

    cra/react-app-rewired - configured react-app-rewired basic configuration

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    console.log('Override config');
+
+    return config;
+  }
